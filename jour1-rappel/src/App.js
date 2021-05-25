1efcb673365955bcd35a202e3a9af78581f28b53
Malik H <malik.h@webdevpro.net>
2021-05-25 15:38:07 +0200

    correction

--- jour1-rappel/src/App.js
+++ jour1-rappel/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Bouton from './jour1/Bouton';
 import Titre from './jour1/Titre';
 import Liste from './jour1/Liste';
-
+import Article from './jour1/Article';
+// SASS 
+// import {Article , Liste , Titre } from './jour1';
 function App() {
   return (
     <div className="container">
@@ -21,6 +23,9 @@ function App() {
           </div>
           <div className="col">
             <Liste />
+          </div>
+          <div className="col">
+            <Article />
           </div>
       </section>
     </div>
---
+++ jour1-rappel/src/jour1/Article.jsx
@@ -0,0 +1,8 @@
+const Article = () => (
+    <article>
+        <h2>je suis un titre</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est, suscipit.</p>
+    </article>
+);
+
+export default Article;
