66c83301b28de89736a5e32f9af8f2ca11ae6c87
Malik H <malik.h@webdevpro.net>
2021-05-26 17:05:52 +0200

    validation du formulaire

--- jour1-rappel/src/jour2/Connexion.jsx
+++ jour1-rappel/src/jour2/Connexion.jsx
@@ -1,19 +1,58 @@
 import React from 'react';
 
 class Connexion extends React.Component {
-    state = {  }
+    // initialiser le state lorsque l'on lance le composant
+    state = { 
+        identifiants : {
+            login : "",
+            password : ""
+        }
+    }
+
+    submit = e => {
+        e.preventDefault();
+        // bloquer le rafraichissement automatique de la page pour faire des traitements 
+        // vérifier que les idenfiants sont corrects 
+        // si ok => redirection sur le tableau de bord du back office
+        // si ko => mettre des messages d'erreur
+        console.log(this.state);
+    }
+
+    change = e => {
+        const valeurSaisie = e.currentTarget.value; // info saisie
+        const nameInputUtilise = e.currentTarget.name; // nom du champ utilisé
+        const cloneIdentifiant = {...this.state.identifiants}; // clone
+        cloneIdentifiant[nameInputUtilise] = valeurSaisie;  // modifier le clone 
+        this.setState({
+            identifiants : cloneIdentifiant
+        });// bon café ! rdv 16h15 !! @ toute suite 
+    }
+
     render() { 
+        let {login , password} =  this.state.identifiants; 
         return ( 
             <div className="col d-flex justify-content-center my-5 text-center">
-                <form action="" className="w-25">
+                <form onSubmit={ this.submit } className="w-25">
                     <h1 className="fs-4">Connexion au Back Office</h1>
                     <div className="form-group">
                         <label htmlFor="login">login</label>
-                        <input type="text" className="form-control my-3" id="login" name="login" />
+                        <input 
+                            type="text" 
+                            className="form-control my-3" 
+                            id="login" 
+                            name="login" 
+                            value={login}
+                            onChange={this.change} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">password</label>
-                        <input type="text" className="form-control my-3" id="password" name="password" />
+                        <input 
+                            type="text" 
+                            className="form-control my-3" 
+                            id="password" 
+                            name="password" 
+                            value={password}
+                            onChange={this.change} />
                     </div>
                     <div className="form-group">
                         <input type="submit" className="btn btn-success" />
--- jour1-rappel/src/jour2/Contact.jsx
+++ jour1-rappel/src/jour2/Contact.jsx
@@ -1,8 +1,93 @@
 import { Component } from 'react';
 class Contact extends Component {
-    state = {  }
+    state = { 
+        demande : {
+            nom : "John DOE",
+            email : "",
+            commentaire : ""
+        },
+        errors : {} // stocker le ou les erreurs détectées lors de la saisie du formulaire 
+    }
+    validation = () => {
+        const cloneDemande = {...this.state.demande};
+        const erreurs = {};
+        if( cloneDemande.nom.length < 5 ){
+            erreurs.nom = "le champ nom doit avoir au moins 5 caractères";
+        }
+        const verif 	= /^[a-z]+@[a-z]{2,}[.][a-z]{2,3}$/ ;
+        if( !verif.exec(cloneDemande.email)  ){
+            erreurs.email = "le champ email n'est pas conforme";
+        }
+        if( cloneDemande.commentaire.length < 5 ){
+            erreurs.nom = "le champ commentaire doit avoir au moins 5 caractères";
+        }
+        // si variable erreurs est objet vide => true 
+        // sinon erreurs => récupérer les informations et les afficher à l'écran 
+        //if(Object.keys(erreurs).length ==  0 ){
+        //    return true;
+        //}
+        // return erreurs ;
+        return Object.keys(erreurs).length ==  0  ? true : erreurs ;
+    }
+
+    submit = e => {
+        e.preventDefault();
+
+    }
+    change = e => {
+        const valeurSaisie = e.currentTarget.value;
+        const nomChamp = e.currentTarget.name;
+        const cloneDemande = {...this.state.demande};
+        cloneDemande[nomChamp] = valeurSaisie;
+        this.setState({
+            demande : cloneDemande
+        });
+    }
+
     render() { 
-        return ( <>Je suis le composant Contact</> );
+        const { nom , email , commentaire } = this.state.demande ;
+        return ( 
+            <div className="col">
+                <h1>Nous contacter</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, labore?</p>
+                <form onSubmit={this.submit} className="row">
+                    <div className="col-6">
+                        <label htmlFor="nom">votre nom</label>
+                        <input 
+                            type="text" 
+                            name="nom" 
+                            className="form-control" 
+                            id="nom" 
+                            value={nom}
+                            onChange={this.change}
+                            />
+                    </div>
+                    <div className="col-6">
+                        <label htmlFor="email">votre email</label>
+                        <input 
+                            type="email" 
+                            name="email" 
+                            className="form-control" 
+                            id="email" 
+                            value={email}
+                            onChange={this.change}
+                            />
+                    </div>
+                    <div className="col-12">
+                        <label htmlFor="commentaire">votre commentaire</label>
+                        <textarea 
+                            name="commentaire" 
+                            id="commentaire" 
+                            className="form-control"
+                            value={commentaire}
+                            onChange={this.change}></textarea>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                        <input type="submit" className="btn btn-outline-dark btn-lg my-4" />
+                    </div>
+                </form>
+            </div>
+         );
     }
 }
  
