f9d8b3b03122326a1691c37c7e2e9ddf19587c70
Malik H <malik.h@webdevpro.net>
2021-05-26 14:16:27 +0200

    url variable

--- jour1-rappel/src/jour2/Produit.jsx
+++ jour1-rappel/src/jour2/Produit.jsx
@@ -3,7 +3,12 @@ import { Component } from 'react';
 class Produit extends Component {
     state = {  }
     render() { 
-        return ( <>je suis un produit unique</> );
+        const { id } = this.props.match.params ; /** récupération de id dans l'url */
+        return ( <div className="col">
+            <img src="http://via.placeholder.com/1200x200" alt="" className="img-fluid my-3" />
+            <h2>Produit { id }</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatibus dolorum iste, sint libero cupiditate. Officiis, dignissimos? Delectus doloribus quas officiis excepturi debitis ex numquam quia laborum quidem magni. Corporis?</p>
+        </div> );
     }
 }
  
