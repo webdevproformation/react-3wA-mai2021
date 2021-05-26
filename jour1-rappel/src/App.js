5dff8a5e282dfeb6a8ca8bb2aff6df6063da3539
Malik H <malik.h@webdevpro.net>
2021-05-26 11:52:51 +0200

    gestion du routing

--- jour1-rappel/src/App.js
+++ jour1-rappel/src/App.js
@@ -1,9 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 
+import Home from "./jour2/Home";
+import Contact from "./jour2/Contact";
+import Produits from "./jour2/Produits";
+import Produit from "./jour2/Produit";
+import { Route , Switch } from "react-router-dom";
 
 import NavBar from './jour2/NavBar';
-
 function App() {
   return (
     <div className="container">
@@ -11,7 +15,16 @@ function App() {
           <div className="col">
             <NavBar />
           </div>
-        </header>  
+        </header>
+        <main className="row">
+          <div className="col">
+            <Switch>
+              <Route path="/produits" component={Produits}/>
+              <Route path="/nous-contacter" component={Contact}/>
+              <Route path="/" component={Home} />
+            </Switch>
+          </div>
+        </main>
     </div>
   );
 }
