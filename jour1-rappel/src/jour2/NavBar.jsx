65632ca9aae1b840a39e4cae7e52fd96532e7222
Malik H <malik.h@webdevpro.net>
2021-05-26 12:03:25 +0200

    fin mis à jour du menu

--- jour1-rappel/src/jour2/NavBar.jsx
+++ jour1-rappel/src/jour2/NavBar.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { NavLink , Link } from "react-router-dom";
 
 class NavBar extends Component {
     state = {  }
     render() { 
         return (
             <nav className="navbar navbar-expand navbar-light bg-light px-3">
-                <Link to="/" className="navbar-brand">Jour2</Link>
+                <Link to="/" 
+                    className="navbar-brand" 
+                    >Jour2</Link>
                 <ul className="navbar-nav">
-                    <li className="nav-item"><Link to="/" className="nav-link">Accueil</Link></li>
-                    <li className="nav-item"><Link to="/produits" className="nav-link">Produits</Link></li>
-                    <li className="nav-item"><Link to="/nous-contacter" className="nav-link">Contacter nous</Link></li>
+                    <li className="nav-item">
+                        <NavLink 
+                            to="/" 
+                            className="nav-link" 
+                            activeClassName="active"
+                            exact>
+                            Accueil</NavLink>
+                    </li>
+                    <li className="nav-item" >
+                            <NavLink 
+                                to="/produits" 
+                                className="nav-link"
+                                activeClassName="active">
+                                Produits</NavLink>
+                    </li>
+                    <li className="nav-item">
+                            <NavLink 
+                                to="/nous-contacter" 
+                                className="nav-link"
+                                activeClassName="active">
+                                    Contacter nous</NavLink>
+                    </li>
                 </ul>
             </nav>
         );
